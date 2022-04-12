e7b38579d0dad393884afdd45ec1fd98e731dda6
Southpaw <bagaudinov53@gmail.com>
2022-04-12 13:41:28 +0600

    ready

---
+++ WpfApp1/Model/Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class Product
+    {
+
+        public Object getTextDecor
+        {
+            get
+            {
+                if (getSale != ProductCost)
+                {
+                    return TextDecorations.Strikethrough;
+                }
+                return null;
+            }
+        }
+        public decimal getSale
+        {
+            get
+            {
+                if (ProductDiscountAmount != 0)
+                {
+                    var price = ProductCost.Value * (decimal)(1 - Double.Parse(ProductDiscountAmount.Value.ToString()) / 100);
+                    return price;
+                }
+                return ProductCost.Value;
+            }
+        }
+    }
+}
---
+++ WpfApp1/Model/Instances.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Instances
+    {
+        private static user4Entities _Db = null;
+        public static user4Entities db
+        {
+            get
+            {
+                if (_Db == null)
+                    _Db = new user4Entities();
+                return _Db;
+            }
+        }
+    }
+}
